d4687332143150433f33560fcfedaf261b5956bd
ZeroByter <romgamesrom@gmail.com>
2022-01-24 19:33:20 +0200

    added page title changed favicon.ico

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Head from "next/head"
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Secret Notepad</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
