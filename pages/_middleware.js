b5cda3718a7c0f428f425b5cf6a849b35dae69e6
ZeroByter <romgamesrom@gmail.com>
2022-01-26 00:42:01 +0200

    maybe now https will work?

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 const isProduction = process.env.NODE_ENV === 'production'
 
 export default function handle(req, res){
-    if(isProduction && req.nextUrl.protocol == "http:"){
+    if(isProduction && req.headers.get("x-forwarded-proto") !== "https"){
         return NextResponse.redirect(process.env.SECURE_REDIRECT_URL, 301)
     }
 
