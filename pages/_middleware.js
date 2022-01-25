c4f5de1be8a881fec006e98335ea716bb8f52c70
ZeroByter <romgamesrom@gmail.com>
2022-01-25 22:20:02 +0200

    fixed enforced https not working

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server"
 const isProduction = process.env.NODE_ENV === 'production'
 
 export default function handle(req, res){
-    if(isProduction && req.headers.get("x-forwarded-proto") !== "https"){
-        return NextResponse.redirect(`https://${req.nextUrl.hostname}${req.nextUrl.pathname}`, 301)
+    if(isProduction && req.nextUrl.protocol == "http:"){
+        return NextResponse.redirect(req.nextUrl.href.replace(/^http/, "https"), 301)
     }
 
     return NextResponse.next()
