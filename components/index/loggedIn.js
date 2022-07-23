8d50f51eb71d8f0d09a19a9ad6e26595251e46c5
ZeroByter <romgamesrom@gmail.com>
2022-07-23 20:48:38 +0300

    fixed folders not refreshing when creating new folder

--- components/index/loggedIn.js
+++ components/index/loggedIn.js
@@ -4,9 +4,11 @@ import Text from "./text";
 import css from "./loggedIn.module.scss"
 import NewFolderButton from "./newFolder/newFolderButton";
 import Folder from "./folder";
+import { useSSRFetcher } from "../contexts/ssrFetcher";
 
 export default function LoggedIn({ texts, isRootFolder, folders, parentFolderId }) {
     const router = useRouter()
+    const ssrFetcher = useSSRFetcher()
 
     let renderGoBackFolder = null
     if (!isRootFolder) {
@@ -23,6 +25,15 @@ export default function LoggedIn({ texts, isRootFolder, folders, parentFolderId 
         return <Text key={text.id} text={text} />
     })
 
+    const handleOnCreated = (_title, _encryptTitle, _titleHint, newFolders) => {
+        ssrFetcher.setProps(oldState => {
+            return {
+                ...oldState,
+                folders: newFolders
+            }
+        })
+    }
+
     let newTextUrl = "/newText"
     if (router.query.id) {
         newTextUrl = `/newText#${router.query.id}`
@@ -32,7 +43,7 @@ export default function LoggedIn({ texts, isRootFolder, folders, parentFolderId 
         <div>
             <div className={css.controlsContainer}>
                 <Link href={newTextUrl}>new text</Link>
-                <NewFolderButton />
+                <NewFolderButton onCreated={handleOnCreated} />
             </div>
             <div className={css.textsContainer}>
                 {renderGoBackFolder}
--- pages/api/folders/create.js
+++ pages/api/folders/create.js
@@ -1,7 +1,7 @@
 import { getLoginSession } from "../../../serverlib/auth"
 import FoldersSQL from "../../../serverlib/sql-classes/folders"
 
-export default async function handle(req, res){
+export default async function handle(req, res) {
     const session = await getLoginSession(req)
 
     if (session == null) {
@@ -11,29 +11,29 @@ export default async function handle(req, res){
         return
     }
 
-    if(req.body.title == null){
+    if (req.body.title == null) {
         res.send({
             error: "title is missing"
         })
         return
     }
-    if(req.body.titleHint == null){
+    if (req.body.titleHint == null) {
         res.send({
             error: "titleHint is missing"
         })
         return
     }
-    if(req.body.encryptTitle == null){
+    if (req.body.encryptTitle == null) {
         res.send({
             error: "encryptTitle is missing"
         })
         return
     }
 
-    if(req.body.parent != null){
-        const folder = await FoldersSql.getById(req.body.parent)
+    if (req.body.parent != null) {
+        const folder = await FoldersSQL.getById(req.body.parent)
 
-        if(folder == null || folder.userid != session.id){
+        if (folder == null || folder.userid != session.id) {
             res.send({
                 error: "parent is not a valid folder"
             })
@@ -41,7 +41,7 @@ export default async function handle(req, res){
         }
     }
 
-    const newId = await FoldersSQL.create(session.id, req.body.parent, req.body.title, req.body.encryptTitle, req.body.titleHint)
+    await FoldersSQL.create(session.id, req.body.parent, req.body.title, req.body.encryptTitle, req.body.titleHint)
 
     const folders = await FoldersSQL.getAllInParent(session.id, req.body.parent)
 
